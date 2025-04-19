c5bdefccfa7bd9af9e7548c6458193dbe10efb0f
Slayder <slayderreyescuellar@gmail.com>
2025-04-19 17:50:12 -0500

    webapck and SASS finish

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    js: './src/app.js', // Punto de entrada para tu JavaScript
+    css: './src/sass/main.scss' // Punto de entrada para tu SCSS
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'main.js',
+    filename: '[name].js', // Generará main.js y css.js (este último no lo usaremos directamente)
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader, // Extrae el CSS a un archivo separado
+          'css-loader', // Interpreta @import y url() como import/require()
+          'sass-loader' // Compila Sass a CSS
+        ],
       },
+      // Aquí puedes añadir reglas para otros tipos de archivos (JS, imágenes, etc.)
     ],
   },
-  mode: 'development',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'theme.css', // El nombre del archivo CSS generado en la carpeta 'public'
+    }),
+  ],
+  mode: 'development', // Puedes cambiar a 'production' para optimizaciones
 };
